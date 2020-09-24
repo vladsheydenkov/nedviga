5c21916c304dc3b4e808e3cc648fdd11c23c619f
nonmonarch <mikita.monarkha@yandex.by>
2020-09-24 20:36:22 +0300

    heroku

--- nedviga/settings.py
+++ nedviga/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['nedviga-app.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['nedviga-app.herokuapp.com', '127.0.0.1']
 #AUTH_USER_MODEL = 'users.User'
 
 
