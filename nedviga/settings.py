735f6be9d937d863a12a94dc997469b369740737
nonmonarch <mikita.monarkha@yandex.by>
2020-09-24 16:59:24 +0300

    whitenoise add

--- nedviga/settings.py
+++ nedviga/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -133,9 +134,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/assets/'
-STATICFILES_DIRS = (
+STATICFILES_DIRS = [
 os.path.join(BASE_DIR, 'assets'),
-)
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
